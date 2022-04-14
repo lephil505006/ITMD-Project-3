2fb9b7de4ddc61e0be0f18d85a5aa392612d09da
lephil505006 <lephil505006@gmail.com>
2022-04-14 16:06:03 -0500

    Title and editing changes

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 function initMap() {
   const cantho = { lat: 10.0452, lng: 105.7469 };
   const danang = { lat: 16.0545, lng: 108.1717 };
+  const hanoi = { lat: 21.0278, lng: 105.8342 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
     center: cantho,
   });
+  
   const marker = new google.maps.Marker({
     position: cantho,	
     map: map,
